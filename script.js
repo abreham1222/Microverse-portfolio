9a5a467ea24a5fa313d680bf8979221586cc18bf
abrish <97598663+abreham1222@users.noreply.github.com>
2022-04-05 18:22:02 +0300

    Add the eventlistner to files

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const humburger = document.querySelector(".humburger");
 const navMenu = document.querySelector(".navbar-desk");
+
+humburger.addEventListener("click", () => {
+    humburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
