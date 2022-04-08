fe3ce0a2f98d7e4db0c75b9d11bdd0ec8fb3f7db
abrish <97598663+abreham1222@users.noreply.github.com>
2022-04-08 15:02:41 +0300

    Add popup close button for js

---
+++ script.js
@@ -0,0 +1,42 @@
+const humburger = document.querySelector('.humburger');
+const navMenu = document.querySelector('.navbar-desk');
+
+humburger.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-lin').forEach((n) => n.addEventListener('click', () => {
+  humburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
+
+var p = document.querySelector('.modal-mobile'),
+  b = document.querySelector('.project'),
+  c = document.querySelector('.see-proj'),
+  Y = document.querySelector('.xx'),
+  section = document.querySelector('section');
+
+b.addEventListener('click', appe);
+c.addEventListener('click', appe);
+
+function appe() {
+  p.style.display = 'block';
+  section.style.filter = 'blur(10px)'
+  }
+
+Y.addEventListener('click', disappeY);
+
+function disappeY() {
+  p.style.display = 'none';
+  section.style.filter = "blur(0px)"
+  }
+
+parent.addEventListener('click', disappeP)
+
+function disappeP(t) {
+  if (t.target.className == 'modal-mobile') {
+    p.style.display = 'none';
+    section.style.filter = 'blur(0px)'
+  }
+}
