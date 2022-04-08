80739419e4b0930fb7f10db3ec958235c2e0cac1
ONJoseph <on.josephinfo@gmail.com>
2022-04-08 17:45:29 +0100

    add window popup queryselector

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const humburger = document.querySelector('.humburger');
 const navMenu = document.querySelector('.navbar-desk');
 
 humburger.addEventListener('click', () => {
-  humburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+    humburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav-lin').forEach((n) => n.addEventListener('click', () => {
-  humburger.classList.remove('active');
-  navMenu.classList.remove('active');
+    humburger.classList.remove('active');
+    navMenu.classList.remove('active');
 }));
 
 const p = document.querySelector('.modal-mobile');
@@ -18,23 +18,33 @@ const Y = document.querySelector('.xx');
 const section = document.querySelector('section');
 
 function appe() {
-  p.style.display = 'block';
-  section.style.filter = 'blur(10px)';
+    p.style.display = 'block';
+    section.style.filter = 'blur(10px)';
 }
 b.addEventListener('click', appe);
 c.addEventListener('click', appe);
 
 function disappeY() {
-  p.style.display = 'none';
-  section.style.filter = 'blur(0px)';
+    p.style.display = 'none';
+    section.style.filter = 'blur(0px)';
 }
 
 Y.addEventListener('click', disappeY);
 
 function disappeP(t) {
-  if (t.target.className === 'modal-mobile') {
-    p.style.display = 'none';
-    section.style.filter = 'blur(0px)';
-  }
+    if (t.target.className === 'modal-mobile') {
+        p.style.display = 'none';
+        section.style.filter = 'blur(0px)';
+    }
 }
 p.addEventListener('click', disappeP);
+
+const parent = document.querySelector('.modal-p');
+const btn = document.querySelector('.see');
+const X = document.querySelector('.x');
+const sect = document.querySelector('section');
+
+function appear() {
+    parent.style.display = 'block';
+    sect.style.filter = 'blur(10px)';
+}
